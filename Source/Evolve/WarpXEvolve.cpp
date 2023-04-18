392055839dbf0efc5597ab888cb19363ec823fe3
Hannah Klion <klion@lbl.gov>
2023-04-18 09:58:31 -0700

    fixed brace issue from merge

--- Source/Evolve/WarpXEvolve.cpp
+++ Source/Evolve/WarpXEvolve.cpp
@@ -1008,11 +1008,12 @@ WarpX::PushParticlesandDepose (int lev, amrex::Real cur_time, DtType a_dt_type, 
         // of the filter to avoid incorrect results (moved to `SyncCurrentAndRho()`).
         // Might this be related to issue #1943?
 #endif
-    if ((! skip_deposition) && (do_subcycle_current)) {
-        if (WarpX::current_deposition_algo == CurrentDepositionAlgo::Vay)
-            ApplySubcyclingScalingToCurrentDensity(current_fp_vay[lev][0].get(), current_fp_vay[lev][1].get(), current_fp_vay[lev][2].get(), n_subcycle_current, lev);
-        else
-            ApplySubcyclingScalingToCurrentDensity(current_fp[lev][0].get(), current_fp[lev][1].get(), current_fp[lev][2].get(), n_subcycle_current, lev);
+        if ((do_subcycle_current)) {
+            if (WarpX::current_deposition_algo == CurrentDepositionAlgo::Vay)
+                ApplySubcyclingScalingToCurrentDensity(current_fp_vay[lev][0].get(), current_fp_vay[lev][1].get(), current_fp_vay[lev][2].get(), n_subcycle_current, lev);
+            else
+                ApplySubcyclingScalingToCurrentDensity(current_fp[lev][0].get(), current_fp[lev][1].get(), current_fp[lev][2].get(), n_subcycle_current, lev);
+        }
     }
 }
 
