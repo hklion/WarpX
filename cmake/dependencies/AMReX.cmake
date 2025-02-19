hklion/WarpX

commit 17692a04f5e4f24c4feb85013ff5da25523ee713

Update to latest AMReX version (#5669)
Update to latest AMReX version to pull the latest bug fix in
https://github.com/AMReX-Codes/amrex/pull/4333.

.github/workflows/cuda.yml
@@ -127,7 +127,7 @@ jobs:
         which nvcc || echo "nvcc not in PATH!"
 
         git clone https://github.com/AMReX-Codes/amrex.git ../amrex
-        cd ../amrex && git checkout --detach 198da4879a63f1bc8c4e8d674bf9185525318f61 && cd -
+        cd ../amrex && git checkout --detach 275f55f25fec350dfedb54f75a19200b52ced93f && cd -
         make COMP=gcc QED=FALSE USE_MPI=TRUE USE_GPU=TRUE USE_OMP=FALSE USE_FFT=TRUE USE_CCACHE=TRUE -j 4
 
         ccache -s

cmake/dependencies/AMReX.cmake
@@ -294,7 +294,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "198da4879a63f1bc8c4e8d674bf9185525318f61"
+set(WarpX_amrex_branch "275f55f25fec350dfedb54f75a19200b52ced93f"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 

commit 04b0cb1177a9558abe451169406abc40bcd36c79

AMReX/pyAMReX/PICSAR: Weekly Update (#5680)
Weekly update to latest AMReX.
Weekly update to latest pyAMReX (no changes).
Weekly update to latest PICSAR (no changes).

```console
./Tools/Release/updateAMReX.py
./Tools/Release/updatepyAMReX.py
./Tools/Release/updatePICSAR.py
```

This pulls in https://github.com/AMReX-Codes/amrex/pull/4337, fixing
regressions from #5669 (GPU segfaults on particle redistribute)

Signed-off-by: Axel Huebl <axel.huebl@plasma.ninja>

.azure-pipelines.yml
@@ -66,16 +66,6 @@ jobs:
       cacheHitVar: CCACHE_CACHE_RESTORED
     displayName: Cache Ccache Objects
 
-  - task: Cache@2
-    continueOnError: true
-    inputs:
-      key: 'Python3 | "$(System.JobName)" | .azure-pipelines.yml'
-      restoreKeys: |
-         Python3 | "$(System.JobName)" | .azure-pipelines.yml
-      path: /home/vsts/.local/lib/python3.8
-      cacheHitVar: PYTHON38_CACHE_RESTORED
-    displayName: Cache Python Libraries
-
   - bash: |
       set -o nounset errexit pipefail
       cat /proc/cpuinfo | grep "model name" | sort -u
@@ -175,4 +165,10 @@ jobs:
         -exec echo -e "\nBacktrace\n---------\n{}\n---------" \; \
         -exec cat {} \;
     displayName: 'Logs'
+    condition: always()
+
+  - bash: |
+      # clean out so the Post-job Cache "tar" command has more disk space available
+      rm -rf build
+    displayName: 'Clean Build Directory'
     condition: always()

.github/workflows/cuda.yml
@@ -127,7 +127,7 @@ jobs:
         which nvcc || echo "nvcc not in PATH!"
 
         git clone https://github.com/AMReX-Codes/amrex.git ../amrex
-        cd ../amrex && git checkout --detach 275f55f25fec350dfedb54f75a19200b52ced93f && cd -
+        cd ../amrex && git checkout --detach b364becad939a490bca4e7f8b23f7392c558a311 && cd -
         make COMP=gcc QED=FALSE USE_MPI=TRUE USE_GPU=TRUE USE_OMP=FALSE USE_FFT=TRUE USE_CCACHE=TRUE -j 4
 
         ccache -s

cmake/dependencies/AMReX.cmake
@@ -294,7 +294,7 @@ set(WarpX_amrex_src ""
 set(WarpX_amrex_repo "https://github.com/AMReX-Codes/amrex.git"
     CACHE STRING
     "Repository URI to pull and build AMReX from if(WarpX_amrex_internal)")
-set(WarpX_amrex_branch "275f55f25fec350dfedb54f75a19200b52ced93f"
+set(WarpX_amrex_branch "b364becad939a490bca4e7f8b23f7392c558a311"
     CACHE STRING
     "Repository branch for WarpX_amrex_repo if(WarpX_amrex_internal)")
 